5a41c9d66bc412e87869802db33126c986c5d43e
prodosec <prodosecus@gmail.com>
2026-04-12 21:45:22 +0500

    home page again 3

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,15 +2,8 @@ const jwt = require('jsonwebtoken');
 
 async function auth(req, res, next) {
   try {
-    // const authHeader = req.headers.authorization || '';
-    // const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
-    // if (!token) return res.status(401).json({ message: 'Unauthorized' });
-
-    // const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-    // const user = await User.findById(payload.sub).select('-password');
-    // if (!user) return res.status(401).json({ message: 'Unauthorized' });
-    // req.user = user;
-    const token = req.cookies['auth-token'];
+    const authHeader = req.headers.authorization || '';
+    const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
 
     if (!token) {
       return res.status(401).json({
